89f1a71c7fb7e0b0ef2796a6fafc8729acab12dd
MNelson <netmn99@gmail.com>
2022-02-21 13:46:34 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,26 +5,35 @@
 ###              **DETAILS IN README!**               ###
 #########################################################
 
+mkdir ~/bin/
+
+mv ./git_push.sh ~/bin
+
 #create template functions
-echo "note() {cp -r ~/notes/.templates/note_template ./$1} | tee -a ~/.bashrc ~/.zshrc > /dev/null"
+echo "alias note='cp -r ~/notes/.templates/note_template'" | tee -a ~/.bashrc ~/.zshrc >/dev/null
 
-echo "report() {cp -r ~/notes/.templates/report_template ./$1} | tee -a ~/.bashrc ~/.zshrc > /dev/null"
+echo "alias report='cp -r ~/notes/.templates/report_template'" | tee -a ~/.bashrc ~/.zshrc >/dev/null
 
-echo "tool() {cp ~/notes/.templates/tool_template.md ~/notes/tools/$1} | tee -a ~/.bashrc ~/.zshrc > /dev/null"
+echo "alias tool='cp ~/notes/.templates/tool_template.md ~/notes/tools/'" | tee -a ~/.bashrc ~/.zshrc >/dev/null
 
-echo "ttp() {cp ~/notes/.templates/ttp_template.md ~/notes/ttps/$1} | tee -a ~/.bashrc ~/.zshrc > /dev/null"
+echo "alias ttp='cp ~/notes/.templates/ttp_template.md ~/notes/ttps/'" | tee -a ~/.bashrc ~/.zshrc >/dev/null
 
-#create quick note reference function
-echo "toolbox() {cat ~/notes/tools/$1 ~/notes/ttps/$1 2>/dev/null} | tee -a ~/.bashrc ~/.zshrc > /dev/null"
-
-#ask user for git repo url
-read -p "Enter URL to your Git repository: " git_repository
+#create quick note list function
+echo "alias toolbox='find ~/notes/ttps ~/notes/tools'" | tee -a ~/.bashrc ~/.zshrc >/dev/null
 
 #write out current crontab
 crontab -l > mycron
 
+read -p "What is your GitHub username? " uname
+
+read -p "What is your GitHub repository name?" repo
+
+git remote set-url origin git@github.com:$uname/$repo.git
+
+ssh git@github.com
+
 #add git push at 15 minute interval to crontab
-echo "15 * * * * git add ~/notes/* && git commit -m 'auto push' && git push origin master" >> mycron
+echo "*/5 * * * * ~/bin/git_push.sh" >> mycron
 
 #install new cron file
 crontab mycron
